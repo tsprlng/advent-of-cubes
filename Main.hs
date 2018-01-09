026dcd632cc1c7cbd80e8301278629a965a22eba
Tom Spurling <tom@tspurling.co.uk>
2018-01-09 18:27:49 -0500

    More pointless formatting

--- Main.hs
+++ Main.hs
@@ -4,5 +4,8 @@ import Solver
 import Data.List (intercalate)
 
 main = do
-  putStrLn . show $ length allColorPossibilities
+  mapM_ (\n -> putStrLn $ (("Color " ++ show n ++ ": ") ++) $ show $ length $ possibilities n) [0..5]
+  putStrLn ""
+  putStrLn $ ("One of each: " ++) $ show $ length allColorPossibilities
+  putStrLn ""
   putStrLn $ intercalate "\n--\n" $ map show $ allColorPossibilities
