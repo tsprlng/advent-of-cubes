038bba05033a5beb864a00d41d67291f6a2a0329
Tom Spurling <tom@tspurling.co.uk>
2018-01-09 17:27:27 +0000

    allColorPossibilities

--- Solver.hs
+++ Solver.hs
@@ -1,8 +1,9 @@
 import Pieces
-import Data.List (intercalate)
+import Data.List (intercalate, groupBy)
 import Debug.Trace (trace)
 
 newtype Net = Net [Piece]
+type PieceVariations = [Piece]
 
 instance Show Net where
   show (Net [a,b,c,d,e,f]) = intercalate "\n" $ map (foldl1 append) [
@@ -52,32 +53,40 @@ variations (Piece ((c,s,l), vxs)) = (map (\vxs -> Piece ((c,s,l), vxs)) $ rots v
     rots vxs = take 4 $ iterate (rotate 4) vxs
     flip vxs = head vxs : reverse (tail vxs)
 
+groupsOfColorPieces = groupBy (\(Piece ((c1,_,_),_)) (Piece ((c2,_,_),_)) -> c1==c2) pieces
+
+allColorPossibilities :: [Net]
+allColorPossibilities = concatMap tryPiece (head groupsOfColorPieces)
+  where
+    tryPiece p = map (Net . (p:)) $ possibilities2 p $ map (concatMap variations) (tail groupsOfColorPieces)
+
+
 possibilities :: Color -> [Net]
-possibilities color = map (Net . (head thisColor :)) $ possibilities2 (head thisColor) (tail thisColor)
+possibilities color = map (Net . (head thisColor :)) $ possibilities2 (head thisColor) (map variations $ tail thisColor)
   where
     thisColor = filter (\(Piece ((c,s,l),_))->c==color) pieces
 
-possibilities2 :: Piece -> [Piece] -> [[Piece]]
+possibilities2 :: Piece -> [PieceVariations] -> [[Piece]]
 possibilities2 a nexts = concatMap tryVariations $ choices nexts
   where
-    tryVariations :: (Piece, [Piece]) -> [[Piece]]
-    tryVariations (one, others) = concatMap tryVariation $ variations one
+    tryVariations :: (PieceVariations, [PieceVariations]) -> [[Piece]]
+    tryVariations (variations, others) = concatMap tryVariation variations
       where
         tryVariation b
           | interfaces2 (reverse $ edges a !! 2) (edges b !! 0) = map (b:) $ possibilities3a a b others
           | otherwise = []
 
-possibilities3a :: Piece -> Piece -> [Piece] -> [[Piece]]
+possibilities3a :: Piece -> Piece -> [PieceVariations] -> [[Piece]]
 possibilities3a a b nexts = concatMap tryVariations $ choices nexts
   where
-    tryVariations (one, others) = concatMap tryVariation $ variations one
+    tryVariations (variations, others) = concatMap tryVariation variations
       where
         tryVariation c = if valid c then map (c:) $ possibilities3b a b c others else []
         valid c = interfaces3 (edges a !! 3) (reverse $ edges c !! 1) (reverse $ edges b !! 3) (edges c !! 2)
 
 possibilities3b a b c nexts = concatMap tryVariations $ choices nexts
   where
-    tryVariations (one, others) = concatMap tryVariation $ variations one
+    tryVariations (variations, others) = concatMap tryVariation variations
       where
         tryVariation d = if valid d then map (d:) $ possibilities4 a b c d others else []
         valid d = interfaces3 (reverse $ edges a !! 1) (edges d !! 3) (edges b !! 1) (reverse $ edges d !! 2)
@@ -88,13 +97,13 @@ possibilities3b a b c nexts = concatMap tryVariations $ choices nexts
 
 possibilities4 a b c d nexts = concatMap tryVariations $ choices nexts
   where
-    tryVariations (one, others) = concatMap tryVariation $ variations one
+    tryVariations (variations, others) = concatMap tryVariation variations
       where
         tryVariation e = if valid1 e && valid2 e then map (e:) $ possibilities5 a b c d e others else []
         valid1 e = interfaces3 (reverse $ edges b !! 2) (edges e !! 0) (edges c !! 3) (reverse $ edges e !! 3)
         valid2 e = interfaces3 (edges b !! 2) (reverse $ edges e !! 0) (reverse $ edges d !! 1) (edges e !! 1)
 
-possibilities5 a b c d e [last] = concatMap tryVariation $ variations last
+possibilities5 a b c d e [lastVariations] = concatMap tryVariation lastVariations
   where
     tryVariation f = if valid1 f && valid2 f && valid3 f && valid4 f then [[f]] else []
     valid1 f = interfaces3 (reverse $ edges e !! 2) (edges f !! 0) (edges c !! 0) (reverse $ edges f !! 3)
