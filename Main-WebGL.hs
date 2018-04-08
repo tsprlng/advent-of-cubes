9f16c15d018b1827f4ec6c25232906a5108a4b9c
Tom Spurling <tom@tspurling.co.uk>
2018-04-08 22:40:06 +0100

    Progress on the web cube

--- Main-WebGL.hs
+++ Main-WebGL.hs
@@ -4,8 +4,10 @@ import Haste.DOM
 import Haste.Foreign
 import Data.IORef
 
+import Foreign.C.Types (CDouble)
+
 import Solver (allColorPossibilities, netPieces)
-import Cube (transforms, pieceToQuads)
+import Cube (transforms, pieceToQuads, Vertex)
 
 import Haste.DOM
 mkTitle :: String -> IO Elem
@@ -14,16 +16,24 @@ mkTitle text = do
   title <- newElem "h1" `with` [ children [textElem] ]
   return title
 
+v3c :: (CDouble, CDouble, CDouble) -> (Double, Double, Double)
+v3c (x,y,z) = (realToFrac x, realToFrac y, realToFrac z)
+
 main :: IO ()
 main = do
   titleElem <- mkTitle "Hello World"
   appendChild documentBody titleElem
+  render
 
 render = do
   let pcs = netPieces $ head allColorPossibilities
-  mapM_ (zip pcs Cube.transforms) $ \(piece,transform) -> do
+  (flip mapM_) (zip pcs Cube.transforms) $ \(piece,transform) -> do
     let (faces, sides) = pieceToQuads piece
-    addMeshFromQuads $ map transform $ faces
+    (flip addMeshFromQuads "#cc8844") $ map v3c $ map transform $ faces
+    (flip addMeshFromQuads "#eecc88") $ map v3c $ map transform $ sides
 
 alert :: String -> IO ()
 alert = ffi "((s)=>{window.alert(s)})"
+
+addMeshFromQuads :: [(Double, Double, Double)] -> String -> IO ()
+addMeshFromQuads = ffi "addMeshFromQuads"
