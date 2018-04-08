29f9d20f6a6c4e3d1ee8f0cf077e0f65d6fbe74e
Tom Spurling <tom@tspurling.co.uk>
2018-04-08 19:26:04 +0100

    hmmmm

--- Main-WebGL.hs
+++ Main-WebGL.hs
@@ -1,7 +1,13 @@
-import Haste
+{-# LANGUAGE OverloadedStrings #-}
+import Haste hiding (alert)
 import Haste.DOM
+import Haste.Foreign
 import Data.IORef
 
+import Solver (allColorPossibilities, netPieces)
+import Cube (transforms, pieceToQuads)
+
+import Haste.DOM
 mkTitle :: String -> IO Elem
 mkTitle text = do
   textElem <- newTextElem text
@@ -12,3 +18,12 @@ main :: IO ()
 main = do
   titleElem <- mkTitle "Hello World"
   appendChild documentBody titleElem
+
+render = do
+  let pcs = netPieces $ head allColorPossibilities
+  mapM_ (zip pcs Cube.transforms) $ \(piece,transform) -> do
+    let (faces, sides) = pieceToQuads piece
+    addMeshFromQuads $ map transform $ faces
+
+alert :: String -> IO ()
+alert = ffi "((s)=>{window.alert(s)})"
