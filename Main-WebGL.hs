2137c1470cb4afd170e0e2f630fdae5fa7d0c833
Tom Spurling <tom@tspurling.co.uk>
2019-06-03 22:15:29 +0100

    Concision

--- Main-WebGL.hs
+++ Main-WebGL.hs
@@ -4,6 +4,7 @@ import Haste
 import Haste.DOM
 import Haste.Foreign
 import Data.IORef
+import Control.Monad (forM_)
 
 import Numeric (showHex)
 
@@ -24,15 +25,10 @@ main = do
 render :: IO ()
 render = do
   let pcs = netPieces $ head allColorPossibilities
-  (flip mapM_) (zip pcs $ Cube.transforms 1) $ \(piece,transform) -> do
-    let (frontFace, backFace, sides) = pieceToQuads piece
-    (flip addMeshFromQuads (cssColor $ faceColor piece)) $ map transform $ backFace
-  (flip mapM_) (zip pcs $ Cube.transforms 1) $ \(piece,transform) -> do
-    let (frontFace, backFace, sides) = pieceToQuads piece
-    (flip addMeshFromQuads (cssColor $ sideColor piece)) $ map transform $ sides
-  (flip mapM_) (zip pcs $ Cube.transforms 1) $ \(piece,transform) -> do
-    let (frontFace, backFace, sides) = pieceToQuads piece
-    (flip addMeshFromQuads (cssColor $ faceColor piece)) $ map transform $ frontFace
+  let (frontFace, backFace, sides) = (\(fF,_,_)->fF, \(_,bF,_)->bF, \(_,_,s)->s)
+  forM_ [(backFace, faceColor), (sides, sideColor), (frontFace, faceColor)] $ \(quadFilter, colorer) -> do
+    (flip mapM_) (zip pcs $ Cube.transforms 1) $ \(piece,transform) -> do
+      (flip addMeshFromQuads (cssColor $ colorer piece)) $ map transform $ quadFilter $ pieceToQuads piece
 
 addMeshFromQuads :: [(Double, Double, Double)] -> CssColor -> IO ()
 addMeshFromQuads = ffi "(quads, color)=>{ postMessage({quads: quads, color: color}); };"
