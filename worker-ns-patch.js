4596841ea436e5cba3ec7ae61a745297b3157d3e
Tom Spurling <tom@tspurling.co.uk>
2019-06-03 22:24:28 +0100

    fixup! Get it to compile into working state

--- worker-ns-patch.js
+++ worker-ns-patch.js
@@ -1,1 +1,3 @@
 var global = self;
+// window is not accessible when we're a web worker, but haste uses it a lot.
+// so, set the equivalent 'global', which haste also tries, to the equivalent 'self', which works
